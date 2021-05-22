00947a9e3d541194c72f2fb0ae2df87e61591287
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-05-21 17:35:31 -0700

    Create 1173. Immediate Food Delivery I.sql
    Also can use AVG()

---
+++ 1173. Immediate Food Delivery I.sql
@@ -0,0 +1,9 @@
+select 
+    round(
+	(select count(*) from delivery where order_date = customer_pref_delivery_date)
+	/ 
+	(select count(*) from delivery) * 100
+	, 2
+	) 
+	as 
+	immediate_percentage;
