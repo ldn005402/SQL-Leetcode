94c45a621d85ec014ebc59de6bc3f8a582582a8d
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-05-26 12:13:10 -0700

    Create 1435. Create a Session Bar Chart.sql
    union same format table to create chart

---
+++ 1435. Create a Session Bar Chart.sql
@@ -0,0 +1,7 @@
+select "[0-5>" as bin,sum(case when duration/60 between 0 and 5 then 1 else 0 end) total from sessions
+union
+select "[5-10>" ,sum(case when duration/60 between 5 and 10 then 1 else 0 end) from sessions
+union
+select "[10-15>",sum(case when duration/60 between 10 and 15 then 1 else 0 end) from sessions
+union
+select "15 or more" ,sum(case when duration/60 >15 then 1 else 0 end) from sessions
