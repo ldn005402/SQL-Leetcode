4b4db1b58f962d3d23346d33295d88ed090600cc
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-06-10 16:20:45 -0700

    Update 1158. Market Analysis I.sql

--- 1158. Market Analysis I.sql
+++ 1158. Market Analysis I.sql
@@ -1,5 +1,5 @@
 select user_id as buyer_id, join_date, count(order_id) as orders_in_2019
 from users
 left join orders on users.user_id = orders.buyer_id
-and year(order_date) = '2019'
+and year(order_date) = '2018'
 group by user_id
