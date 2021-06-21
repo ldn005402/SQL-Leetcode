2bbda8f4aa240ad2974e2a0057e6fcae8959249c
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-06-20 23:15:06 -0700

    Create 1193. Monthly Transactions I.sql

---
+++ 1193. Monthly Transactions I.sql
@@ -0,0 +1,5 @@
+select left(trans_date,7) as month, country, count(id) as trans_count, sum(state = 'approved') as approved_count, sum(amount) as trans_total_amount, sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+
+from transactions
+group by country, month
+
