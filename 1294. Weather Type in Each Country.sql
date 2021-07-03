dfca60ffa0f0168613b208177aa3cab17362d75f
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-07-03 00:15:18 -0700

    Create 1294. Weather Type in Each Country.sql

---
+++ 1294. Weather Type in Each Country.sql
@@ -0,0 +1,12 @@
+SELECT a.country_name,
+CASE 
+WHEN AVG(weather_state)<=15 THEN "Cold"
+WHEN AVG(weather_State)>=25 THEN "Hot"
+ELSE "Warm"
+END
+AS weather_type 
+FROM Countries AS a
+JOIN Weather AS b
+ON a.country_id = b.country_id
+WHERE MONTH(b.day) = 11
+GROUP BY a.country_id
