113cc716ffa51c49addae5abe521f418b69c7eef
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-07-15 10:18:52 -0700

    Create 1174. Immediate Food Delivery II.sql

---
+++ 1174. Immediate Food Delivery II.sql
@@ -0,0 +1,5 @@
+SELECT ROUND(AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END)*100, 2) AS 'immediate_percentage'
+FROM Delivery
+ 
+WHERE(customer_id, order_date) IN (select customer_id, min(order_date) from Delivery group by customer_id);
+
