00bcc206fbcd589a645a241590eb3c7abbe4c9fd
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-07-18 22:03:10 -0700

    Create 571. Find Median Given Frequency of Numbers.sql

---
+++ 571. Find Median Given Frequency of Numbers.sql
@@ -0,0 +1,4 @@
+select  avg(n.Number) median
+from Numbers n
+where n.Frequency >= abs((select sum(Frequency) from Numbers where Number<=n.Number) -
+                         (select sum(Frequency) from Numbers where Number>=n.Number))
