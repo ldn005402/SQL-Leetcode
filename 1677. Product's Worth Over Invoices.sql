7c03e1aa3e092779be513e6c3331f516ffbd23a3
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-07-29 10:02:40 -0700

    Create 1677. Product's Worth Over Invoices.sql

---
+++ 1677. Product's Worth Over Invoices.sql
@@ -0,0 +1,6 @@
+SELECT p.name, IFNULL(SUM(rest),0) AS rest, IFNULL(SUM(paid),0) AS paid, 
+IFNULL(SUM(canceled),0) AS canceled, IFNULL(SUM(refunded),0) AS refunded
+FROM product p 
+LEFT JOIN invoice i ON p.product_id = i.product_id
+GROUP BY p.name
+ORDER BY p.name
